5c834087d80e8f428a80362268a0822fe44e0481
Mirzet Pidic <mirza.pidic@gmail.com>
2026-04-11 13:15:31 +0200

    added 192x192 favicon for google

--- public/404.php
+++ public/404.php
@@ -16,6 +16,7 @@
     <meta property="og:type" content="website">
 
     <link rel="icon" href="/assets/images/favicon.ico" sizes="any">
+    <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/android-chrome-192x192.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
--- public/index.php
+++ public/index.php
@@ -19,6 +19,7 @@
     <meta property="og:image" content="https://oleria.at/assets/images/group_photo_desktop.webp">
 
     <link rel="icon" href="/assets/images/favicon.ico" sizes="any">
+    <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/android-chrome-192x192.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
